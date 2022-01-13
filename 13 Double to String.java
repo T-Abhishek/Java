2367f31bb5d5cf7229b5e0b1938df3e03dc643e2
Abhishek Thatithuru <82454618+T-Abhishek@users.noreply.github.com>
2022-01-13 19:02:20 +0530

    Create 13 Double to String.java

---
+++ 13 Double to String.java
@@ -0,0 +1,21 @@
+// Java program to convert double data to a string data
+
+// Importing Libraries
+import java.util.*;
+import java.io.*;
+
+class GFG {
+
+	// Main driver function
+	public static void main(String[] args)
+	{
+		// Declaring the number
+		double number = 123.456;
+
+		// Converting Double data to String data
+		String output = String.valueOf(number);
+
+		// Printing the string data
+		System.out.println(output);
+	}
+}
