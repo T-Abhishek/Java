b99ddcb76ff18cb4c2b951873c58b5a20fdf239b
Abhishek Thatithuru <82454618+T-Abhishek@users.noreply.github.com>
2022-02-11 22:50:06 +0530

    Create Current Working Directory.java

---
+++ Current Working Directory.java
@@ -0,0 +1,32 @@
+// Java Program to Find  
+
+// current working directory 
+
+import java.io.*; 
+
+  
+
+public class GFG { 
+
+    
+
+    public static void main(String[] args) 
+
+    { 
+
+  
+
+        // Getting the path of system property 
+
+        String path = System.getProperty("user.dir"); 
+
+  
+
+        // Printing the path of the working Directory 
+
+        System.out.println("Working Directory = " + path); 
+
+    } 
+
+}
+
